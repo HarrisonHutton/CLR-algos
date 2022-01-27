30f720783ec676d478c68e65b2e631447215b291
HarrisonHutton <harrison.hutton6@gmail.com>
2022-01-27 17:24:34 -0500

    Fix parenthesis errors in tests

--- src/chapter2/insertionsort/test_Insertionsort.py
+++ src/chapter2/insertionsort/test_Insertionsort.py
@@ -10,28 +10,28 @@ class test_Insertionsort(unittest.TestCase):
     def test_one_value(self):
         arr = [3]
         insertionsort(arr)
-        self.assertEqual(insertionsort(arr, [3])
+        self.assertEqual(arr, [3])
 
     def test_two_sorted_values(self):
         arr = [2,4]
         insertionsort(arr)
-        self.assertEqual(insertionsort(arr, [2,4])
+        self.assertEqual(arr, [2,4])
 
     def test_two_unsorted_values(self):
         arr = [5,1]
         insertionsort(arr)
-        self.assertEqual(insertionsort(arr, [1,5])
+        self.assertEqual(arr, [1,5])
 
     def test_three_sorted_values(self):
         arr = [1,5,76]
         insertionsort(arr)
-        self.assertEqual(insertionsort(arr, [1,5,76])
+        self.assertEqual(arr, [1,5,76])
 
     # Input will be partially sorted
     def test_three_unsorted_values(self):
         arr = [35,7,23]
         insertionsort(arr)
-        self.assertEqual(insertionsort(arr, [7,23,35])
+        self.assertEqual(arr, [7,23,35])
 
 
 if __name__ == "__main__":
