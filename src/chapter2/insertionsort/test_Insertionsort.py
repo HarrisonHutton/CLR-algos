1c4689bcb111a2bbdc3474117092ee9c91d1ff60
HarrisonHutton <harrison.hutton6@gmail.com>
2022-01-27 17:09:48 -0500

    Write extensive tests

--- src/chapter2/insertionsort/test_Insertionsort.py
+++ src/chapter2/insertionsort/test_Insertionsort.py
@@ -5,6 +5,22 @@ class test_Insertionsort(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(insertionsort([]), [])
 
+    def test_one_value(self):
+        self.assertEqual(insertionsort([3]), [3])
+
+    def test_two_sorted_values(self):
+        self.assertEqual(insertionsort([2,4]), [2,4])
+
+    def test_two_unsorted_values(self):
+        self.assertEqual(insertionsort([5,1]), [1,5])
+
+    def test_three_sorted_values(self):
+        self.assertEqual(insertionsort([1,5,76]), [1,5,76])
+
+    # Input will be partially sorted
+    def test_three_unsorted_values(self):
+        self.assertEqual(insertionsort([35,7,23]), [7,23,35])
+
 
 if __name__ == "__main__":
     unittest.main()
