c02deb213a4dd30d4bec022aeb2fd6aa33f79bce
HarrisonHutton <harrison.hutton6@gmail.com>
2022-01-27 17:15:27 -0500

    Refactor tests; insertionsort sorts in place

--- src/chapter2/insertionsort/test_Insertionsort.py
+++ src/chapter2/insertionsort/test_Insertionsort.py
@@ -3,23 +3,35 @@ from insertionsort import insertionsort
 
 class test_Insertionsort(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(insertionsort([]), [])
+        arr = []
+        insertionsort(arr)
+        self.assertEqual(arr, [])
 
     def test_one_value(self):
-        self.assertEqual(insertionsort([3]), [3])
+        arr = [3]
+        insertionsort(arr)
+        self.assertEqual(insertionsort(arr, [3])
 
     def test_two_sorted_values(self):
-        self.assertEqual(insertionsort([2,4]), [2,4])
+        arr = [2,4]
+        insertionsort(arr)
+        self.assertEqual(insertionsort(arr, [2,4])
 
     def test_two_unsorted_values(self):
-        self.assertEqual(insertionsort([5,1]), [1,5])
+        arr = [5,1]
+        insertionsort(arr)
+        self.assertEqual(insertionsort(arr, [1,5])
 
     def test_three_sorted_values(self):
-        self.assertEqual(insertionsort([1,5,76]), [1,5,76])
+        arr = [1,5,76]
+        insertionsort(arr)
+        self.assertEqual(insertionsort(arr, [1,5,76])
 
     # Input will be partially sorted
     def test_three_unsorted_values(self):
-        self.assertEqual(insertionsort([35,7,23]), [7,23,35])
+        arr = [35,7,23]
+        insertionsort(arr)
+        self.assertEqual(insertionsort(arr, [7,23,35])
 
 
 if __name__ == "__main__":
